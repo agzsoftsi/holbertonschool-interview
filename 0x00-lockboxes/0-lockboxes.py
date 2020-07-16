18084c03f60d57f84722339ba6f1c19ba4da0f07
agzsoftsi <agzsoftsi@gmail.com>
2020-07-16 15:49:01 +0000

    Lockboxes Task Completed, Only tested with Intrenet Test

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes
+
+More info:
+- Prototype: def canUnlockAll(boxes)
+- boxes is a list of lists
+- A key with the same number as a box opens that box
+- You can assume all keys will be positive integers
+- The first box boxes[0] is unlocked
+- Return True if all boxes can be opened, else return False
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Description:
+    Write a method that determines if all the boxes can be opened
+    Arguments:
+    boxes --> List of Lists, it contains the boxes with keys
+    Reurn boolean
+    Variables:
+    myKeys --> List, Store the number keys to open boxes
+    key --> integer, key of the myKeys
+    boxKey --> iinteger, key inside of an specific box
+    """
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys:
+                if boxKey < len(boxes):
+                    myKeys.append(boxKey)
+    if len(myKeys) == len(boxes):
+        return True
+    return False
