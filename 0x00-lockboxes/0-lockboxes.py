83d3737786d79a87fc22f08310c9a2fba9139e05
agzsoftsi <agzsoftsi@gmail.com>
2020-07-17 19:18:31 +0000

    Lockboxes Task Completed, Only tested with Intranet Test

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -29,9 +29,8 @@ def canUnlockAll(boxes):
     myKeys = [0]
     for key in myKeys:
         for boxKey in boxes[key]:
-            if boxKey not in myKeys:
-                if boxKey < len(boxes):
-                    myKeys.append(boxKey)
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
     if len(myKeys) == len(boxes):
         return True
     return False
