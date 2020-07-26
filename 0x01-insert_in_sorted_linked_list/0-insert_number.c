5df84ef3b33282167abeeadc37f3476323ae8ec8
agzsoftsi <agzsoftsi@gmail.com>
2020-07-26 01:57:15 +0000

    Task linked list fix standard libraries

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,18 +10,42 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
+	listint_t *newNode;
+	listint_t *currentNode;
 
-	if (head == NULL)
-		return (NULL);
+	/* defining the start of the list */
+	currentNode = *head;
+	/* Allocate Memory to get a new Node*/
+	newNode = malloc(sizeof(listint_t));
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = number;
-	new->next = *head;
-	*head = new;
-
-	return (*head);
+	/**
+	 * Validate if newNode isn't Null
+	 *Then: assigne in first node the new node and next node is Null
+	 *Then validate if next posisition is null and value of new node is >
+	 *assigne a new value for the next node
+	 *if not go through position to find a value greater to be the newcurre
+	 *at last return de value of node or null if not
+	 *
+	 **/
+	if (newNode)
+	{
+		newNode->n = number;
+		newNode->next = NULL;
+		if (*head == NULL || (*head)->n >= newNode->n)
+		{
+			newNode->next = *head;
+			*head = newNode;
+		}
+		else
+		{
+			while (currentNode->next && currentNode->next->n < newNode->n)
+			{
+				currentNode = currentNode->next;
+			}
+			newNode->next = currentNode->next;
+			currentNode->next = newNode;
+		}
+		return (newNode);
+	}
+	return (NULL);
 }
--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -1,5 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
