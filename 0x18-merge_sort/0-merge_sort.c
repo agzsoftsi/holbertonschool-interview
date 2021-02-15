9d083e54a158d6337ca9c06e6e4919c2c85939a5
Carlos Garcia <agzsoftsi@gmail.com>
2021-02-15 13:30:44 -0500

    Add functions to complete the merge process

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -49,3 +49,74 @@ void sort(int *arr, int *l, int *r, int start, int mid, int end)
 		array_iter++;
 	}
 }
+
+/**
+ * merge - Merge subarrays
+ * @array: Pointer to array of integers
+ * @start: First index of left subarray
+ * @mid: Last index of left subarray
+ * @end: Last index of right subarray
+ *
+ * Return: None
+ */
+void merge(int *array, int start, int mid, int end)
+{
+	int left_iter, right_iter;
+	int left_size = mid - start + 1;
+	int right_size = end - mid;
+
+	#pragma GCC diagnostic ignored "-Wvla"
+	int left[left_size];
+	int right[right_size];
+
+	for (left_iter = 0; left_iter < left_size; left_iter++)
+		left[left_iter] = array[start + left_iter];
+	for (right_iter = 0; right_iter < right_size; right_iter++)
+		right[right_iter] = array[mid + right_iter + 1];
+
+	printf("Merging...\n[left]: ");
+	print_array(left, left_size);
+	printf("[right]: ");
+	print_array(right, right_size);
+
+	sort(array, left, right, start, mid, end);
+
+	printf("[Done]: ");
+	print_array(&array[start], left_size + right_size);
+}
+
+/**
+ * split_arrays - Split array into subarrays
+ * @array: Pointer to array of integers
+ * @start: First index of left subarray
+ * @end: Last index of right subarray
+ *
+ * Return: None
+ */
+void split_arrays(int *array, int start, int end)
+{
+	int mid = (start + end - 1) / 2;
+
+	if (start < end)
+	{
+		split_arrays(array, start, mid);
+		split_arrays(array, mid + 1, end);
+
+		merge(array, start, mid, end);
+	}
+}
+
+/**
+ * merge_sort - Sort array in-place using merge sort algorithm
+ * @array: Pointer to array of integers
+ * @size: Number of elements in array
+ *
+ * Return: None
+ */
+void merge_sort(int *array, size_t size)
+{
+	if (array && size >= 2)
+	{
+		split_arrays(array, 0, size - 1);
+	}
+}
