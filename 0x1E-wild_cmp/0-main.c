19897ec434664190196fe4da4352c915eb4adff1
Carlos Garcia <agzsoftsi@gmail.com>
2021-03-31 10:22:37 -0500

    Update 0-main.c

--- 0x1E-wild_cmp/0-main.c
+++ 0x1E-wild_cmp/0-main.c
@@ -1,57 +1,38 @@
-#include <stdlib.h>
+#include "holberton.h"
 #include <stdio.h>
-#include "binary_trees.h"
 
 /**
- * basic_tree - Build a basic binary tree
+ * main - check the code for Holberton School students.
  *
- * Return: A pointer to the created tree
- */
-binary_tree_t *basic_tree(void)
-{
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 128);
-    root->left->right = binary_tree_node(root->left, 54);
-    root->right->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 10);
-    return (root);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-    binary_tree_t *root;
-    int avl;
+	int r;
 
-    root = basic_tree();
-
-    binary_tree_print(root);
-    avl = binary_tree_is_avl(root);
-    printf("Is %d avl: %d\n", root->n, avl);
-    avl = binary_tree_is_avl(root->left);
-    printf("Is %d avl: %d\n", root->left->n, avl);
-
-    root->right->left = binary_tree_node(root->right, 97);
-    binary_tree_print(root);
-    avl = binary_tree_is_avl(root);
-    printf("Is %d avl: %d\n", root->n, avl);
-
-    root = basic_tree();
-    root->right->right->right = binary_tree_node(root->right->right, 430);
-    binary_tree_print(root);
-    avl = binary_tree_is_avl(root);
-    printf("Is %d avl: %d\n", root->n, avl);
-
-    root->right->right->right->left = binary_tree_node(root->right->right->right, 420);
-    binary_tree_print(root);
-    avl = binary_tree_is_avl(root);
-    printf("Is %d avl: %d\n", root->n, avl);
-    return (0);
+	r = wildcmp("main.c", "*.c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m*a*i*n*.*c*");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "main.c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m*c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "ma********************************c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "*");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "***");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m.*c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "**.*c");
+	printf("%d\n", r);
+	r = wildcmp("main-main.c", "ma*in.c");
+	printf("%d\n", r);
+	r = wildcmp("main", "main*d");
+	printf("%d\n", r);
+	r = wildcmp("abc", "*b");
+	printf("%d\n", r);
+	return (0);
 }
